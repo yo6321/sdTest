fd6cb070e6332fe03318f8e0f285a6609859ef71
yo6321 <100665088+yo6321@users.noreply.github.com>
2022-03-01 14:49:59 +0900

    修正テスト

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <h2>mod test</h2>
     </div>
   );
 }
